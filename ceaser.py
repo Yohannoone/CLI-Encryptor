c1069fd06c8c76a93a5024fbadf66f00112536f5
Yohan <moriayohan86@gmail.com>
2025-07-07 18:19:38 +0100

    Finished the ceaser.py, now lets go to the otherssssss

--- ceaser.py
+++ ceaser.py
@@ -4,10 +4,26 @@ def ceaser_cipher(message, shift_number):
     for letter in message.lower():
         if letter == ' ':
             new_message += ' '
-        else:
+        elif letter in alphabet:
             index = alphabet.find(letter)
             new_index = (index + shift_number) % len(alphabet)
             new_letter = alphabet[new_index]
             new_message += new_letter
+        else:
+            new_message += letter
     return new_message
+
+def ceaser_handler():
+    inp_message = input('Write the message you want to encrypt.')
+    shift_inp = int(input('Write your shift number'))
+    enc_dec = int(input('If you want to encrypt type 1, if decrypt type 2.'))
+    if enc_dec == 1:
+        final_shift = shift_inp * 1
+    elif enc_dec == 2:
+        final_shift = shift_inp * -1
+    else:
+        print('Naaaaaah that is wrong ')
+    print(ceaser_cipher(inp_message, final_shift))
+
+ceaser_handler()
 
