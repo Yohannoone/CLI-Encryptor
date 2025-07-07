3c8e78a9d74b2d080f6f01fd82dd34772f23ef58
Yohan <moriayohan86@gmail.com>
2025-07-07 11:12:32 +0100

    first commit after connecting my own repo with github

--- ceaser.py
+++ ceaser.py
@@ -1,3 +1,15 @@
-def ceaser_cipher():
-    ceaser = 'This is Ceaser Cipher'
-    return ceaser
+def ceaser_cipher(message, shift_number):
+    alphabet = "abcdefghijklmnopqrstuvwxyz"
+    new_message = ''
+    for letter in message.lower():
+        if letter == ' ':
+            new_message += ' '
+        else:
+            index = alphabet.find(letter)
+            new_index = (index + shift_number) % len(alphabet)
+            new_letter = alphabet[new_index]
+            new_message += new_letter
+    return new_message
+
+translted = 'AAA'
+print(ceaser_cipher(translted, 3))
